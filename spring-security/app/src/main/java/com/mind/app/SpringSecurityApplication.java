5b845e09f53d024c266808cef777ee7511b9b8bd
MdSaifKh <saifmind107@gmail.com>
2024-04-14 11:38:03 -0700

    added initial set up

---
+++ spring-security/app/src/main/java/com/mind/app/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.mind.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
